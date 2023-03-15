5de2cb0afa0b1272d4d8228c23dfce3fa08c99ad
CriSFV <cristina.f.valverde@gmail.com>
2023-03-15 12:23:04 +0100

    fix: fix api response

--- src/components/App.js
+++ src/components/App.js
@@ -1,39 +1,29 @@
 import '../styles/App.sass';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import GetPodcast from '../services/api';
 
 function App() {
-//   const [data, setData] = useState([]);
+  const [data, setData] = useState([]);
   useEffect(() => {
     GetPodcast().then(response => {
         console.log('desde app', response)
+        setData(response)
     })
   }, []);
 
-  // function printList(){
-  //   data.map(podcast=>{
-  //     return(
-  //       <li>{podcast}</li>
-  //     )
-  //   })
-  // }
+
 
   return (
-    <div>
+    <div className='ppal_container'>
       <header>
         <h1 className="title">Podcaster</h1>
       </header>
       <main>
         <section className="searcher">
+          <span className='searcher__results'>{data.length}</span>
           <label>
             <input type="text" placeholder="Filter podcast"/>
           </label>
-        </section>
-        <ul>
-        {/* {printList()} */}
-        </ul>
-        <section>
-
         </section>
       </main>
     </div>
--- src/services/api.js
+++ src/services/api.js
@@ -1,14 +1,22 @@
 // https://itunes.apple.com/us/rss/toppodcasts/limit=100/genre=1310/json
 
 const GetPodcast = () => {
-  return fetch(`https://api.allorigins.win/get?url=${encodeURIComponent('https://itunes.apple.com/us/rss/toppodcasts/limit=10/genre=1310/json')}`)
+  return fetch(`https://api.allorigins.win/get?url=${encodeURIComponent('https://itunes.apple.com/us/rss/toppodcasts/limit=100/genre=1310/json')}`)
   .then(response => {
     if (response.ok) return response.json()
     throw new Error('Network response was not ok.')
   })
   .then((data) => {
       const resp = JSON.parse(data.contents);
-      return resp.feed.entry
+
+      return resp.feed.entry.map((podcast)=>{
+        return {
+          id: podcast.id.attributes['im:id'],
+          title: podcast['im:name'].label,
+          author: podcast['im:artist'].label,
+          img: podcast['im:image'][2].label
+        }
+      })
   }); 
 }
 export default GetPodcast;
