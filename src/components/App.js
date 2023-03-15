2e0f8fbcd38fb5cd53916a237fc14cd8bf324dd8
CriSFV <cristina.f.valverde@gmail.com>
2023-03-15 17:31:31 +0100

    feat: save data and date in localStorage

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,38 @@
 import '../styles/App.sass';
 import { useEffect, useState } from 'react';
 import getPodcast from '../services/api';
+import localStorage from '../services/localStorage'
 import List from './List';
 
 function App() {
   const [data, setData] = useState([]);
-  const [userSearch, setUserSearch] = useState('')
+  const [userSearch, setUserSearch] = useState('');
+  const localSotorageDate = localStorage.get('date','');
+  const [userDate, setUserDate] = useState(localSotorageDate)
+
+  useEffect(() => {
+    const today = new Date().toLocaleDateString()
+    if(today !== localSotorageDate){
+      setUserDate(today)
+      localStorage.remove('date')
+      localStorage.set('date', userDate)
+    } else{
+      const data = localStorage.get('podcastData','')
+      setData(data)
+    }
+  },[]);
+
   useEffect(() => {
     getPodcast().then(response => {
         setData(response)
+        
+        localStorage.remove('podcastData')
+        localStorage.set('podcastData', response)
     })
-  }, []);
+  }, [userDate]);
+
+
+
   const handleSearch = (ev) =>{
     setUserSearch(ev.currentTarget.value)
   }
@@ -24,7 +46,7 @@ function App() {
       </header>
       <main>
         <section className="searcher">
-          <span className='searcher__results flex_column_center'>{data.length}</span>
+          <span className='searcher__results flex_column_center'>{podcastFiltered.length}</span>
           <label>
             <input className='searcher__input' type="text" placeholder="Filter podcast" onKeyUp={handleSearch}/>
           </label>
